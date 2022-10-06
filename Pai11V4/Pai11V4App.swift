4dc29035d48023ce872c9dffff5c4395e2db3162
luxlinna <luxlinna@gmail.com>
2022-10-06 09:23:49 +0200

    Initial Commit

---
+++ Pai11V4/Pai11V4App.swift
@@ -0,0 +1,17 @@
+//
+//  Pai11V4App.swift
+//  Pai11V4
+//
+//  Created by Linna Lux on 2022-10-06.
+//
+
+import SwiftUI
+
+@main
+struct Pai11V4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
